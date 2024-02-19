24e83ce2678b994de1a7385707edb9ba8f94b89b
code-black04 <purnimamaheshwari021@gmail.com>
2024-02-19 22:49:07 +0000

    removed throws from main function and used try catch

--- RSAKeyGenerator.java
+++ RSAKeyGenerator.java
@@ -3,23 +3,27 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
 public class RSAKeyGenerator {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        if (args.length != 1) {
-            System.err.println("Usage: java RSAKeyGen <<userid>>");
-            System.exit(-1);
+        try {
+            if (args.length != 1) {
+                System.err.println("Usage: java RSAKeyGen <<userid>>");
+                System.exit(-1);
+            }
+
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair kp = kpg.genKeyPair();
+
+            FileOutputStream fos = new FileOutputStream(args[0] + ".pub");
+            fos.write(kp.getPublic().getEncoded());
+            fos.close();
+
+            fos = new FileOutputStream(args[0] + ".prv");
+            fos.write(kp.getPrivate().getEncoded());
+            fos.close();
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
         }
-
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(2048);
-        KeyPair kp = kpg.genKeyPair();
-
-        FileOutputStream fos = new FileOutputStream(args[0] + ".pub");
-        fos.write(kp.getPublic().getEncoded());
-        fos.close();
-
-        fos = new FileOutputStream(args[0] + ".prv");
-        fos.write(kp.getPrivate().getEncoded());
-        fos.close();
     }
 }
